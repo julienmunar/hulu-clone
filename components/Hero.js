7d0c7a97911a3c316abfdc1a3bdf56190e75e84b
Munar <a876282@eu.eurocopter.corp>
2022-06-18 16:25:44 +0200

    'first'

--- components/Hero.js
+++ components/Hero.js
@@ -6,11 +6,11 @@ const Hero = () => {
     return (
         <div>
             <div className='text-center pt-4'>
-                <p className='text-[#1CE783] font-bold pb-6 text-xs'>BUNDLE WITH ANY HULU PLAN  SAVE</p>
+                <p className='text-[#1CE783] font-bold pb-6 text-xs'>BUNDLE WITH ANY HULU PLANSAVE</p>
                 <Image src={Img} width={603} height={136} alt='ImageWallpaper'  className="pb-8" />
                 <p  className=' text-white text-2xl font-semibold pb-6'>Get endless entertainment, live sports, and the shows and <br /> movies you love.</p>
                 <button className=' bg-[#1CE783] py-3  px-60 rounded-md font-semibold hover:opacity-30 transition-opacity'>GET THE DISNEY BUNDLE</button>
-                <p className='text-gray-400 pt-4  font-bold text-xs pb-3'><span className='underline'>What's included?</span> See <span className='underline'>Bundle terms.</span></p>
+                <p className='text-gray-400 pt-4  font-bold text-xs pb-3'><span className='underline'>What included?</span> See <span className='underline'>Bundle terms.</span></p>
                 <p className=' text-white underline text-md pb-20'>Sign up for Hulu only</p>
             </div>
 
